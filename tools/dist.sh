498dfc58e26aedcdd276df5df62f4393d3a9bf40
Jim Ramsay <i.am@jimramsay.com>
2020-08-05 06:37:36 -0400

    Alter dist-building scripts to use egg_info instead of sed to add version tags

--- tools/dist.sh
+++ tools/dist.sh
@@ -6,14 +6,8 @@ fi
 rm -rf dist build soundcraft_utils.egg-info
 if [[ $VERSION_SUFFIX ]]; then
     echo "Overriding version with $VERSION_SUFFIX"
-    ORIG=soundcraft/__init__.py
-    BAK=${ORIG}.bak
-    if [[ -f $BAK ]]; then
-        echo "$BAK already exists"
-        exit 1
-    fi
-    sed -i.bak -e "s/^\\(__version__ = ['\"][^'\"]*\\)/\\1$VERSION_SUFFIX/" $ORIG
+    EGG_INFO="egg_info -b $VERSION_SUFFIX"
 fi
-python3 setup.py sdist bdist_wheel || exit 1
+python3 setup.py $EGG_INFO sdist bdist_wheel || exit 1
 [[ -f $BAK ]] && mv $BAK $ORIG
 exit 0
--- tools/release.sh
+++ tools/release.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 TOOLS=$(dirname $0)
-grep version soundcraft/__init__.py
+$TOOLS/dist.sh || exit 1
+echo ”----------------------------------------”
+ls dist
 echo -n "About to release; are you sure? [y/N] "
 read answer
 if [[ ! $answer =~ ^[yY] ]]; then
     echo "Exiting"
     exit 1
 fi
-$TOOLS/dist.sh || exit 1
 python3 -m twine upload --user __token__ --password $(<$TOOLS/pypi.realkey) dist/*
