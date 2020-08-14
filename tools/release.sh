9c3aada880479876fff7bd74e02c37f28640187a
Jim Ramsay <i.am@jimramsay.com>
2020-08-14 04:34:19 -0400

    Release script cleanup
    Github now publishes the official releases, so these scripts are only
    needed for pre-release publishing.

--- tools/release.sh
+++ tools/release.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 TOOLS=$(dirname $0)
+export VERSION_SUFFIX=$1
+if [[ ! $VERSION_SUFFIX ]]; then
+    echo "usage: $0 [a1|b1|rc1]"
+    exit 1
+fi
+if [[ ! $VERSION_SUFFIX =~ ^(a|b|rc)[0-9]+$ ]]; then
+    echo "Suffix must be one of:"
+    echo "  a2"
+    echo "  b5"
+    echo "  rc1"
+    exit 2
+fi
 $TOOLS/dist.sh || exit 1
 echo ”----------------------------------------”
 ls dist
