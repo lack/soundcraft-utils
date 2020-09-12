4a7a369eba53b30fb0d72fa8ebf11c7513df177a
Hans Ulrich Niedermann <hun@n-dimensional.de>
2020-09-12 22:11:11 +0200

    contrib: Use busctl variable for busctl command
    This allows us to change that command in a single place if required.

--- contrib/dbus-stuff/start-dbus-service.sh
+++ contrib/dbus-stuff/start-dbus-service.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+busctl="busctl"
+
 busname="soundcraft.utils.notepad"
 
 set -xe
@@ -10,7 +12,7 @@ set -xe
 #	  /org/freedesktop/DBus org.freedesktop.DBus.StartServiceByName \
 #	  "string:${busname}" uint32:0
 
-busctl call org.freedesktop.DBus /org/freedesktop/DBus \
+${busctl} call org.freedesktop.DBus /org/freedesktop/DBus \
        org.freedesktop.DBus StartServiceByName su "${busname}" 0
 
 sleep 2
@@ -18,14 +20,14 @@ sleep 2
 # See what objects the soundcraft-utils dbus service exposes on the system bus
 # Note that busctl has beautiful bash shell completion for bus names etc.
 
-busctl tree "${busname}"
-busctl --no-pager introspect "${busname}" /soundcraft/utils/notepad/0
+${busctl} tree "${busname}"
+${busctl} --no-pager introspect "${busname}" /soundcraft/utils/notepad/0
 
-busctl get-property "${busname}" /soundcraft/utils/notepad/0 \
+${busctl} get-property "${busname}" /soundcraft/utils/notepad/0 \
        soundcraft.utils.notepad.device sources
 
-busctl get-property "${busname}" /soundcraft/utils/notepad/0 \
+${busctl} get-property "${busname}" /soundcraft/utils/notepad/0 \
        soundcraft.utils.notepad.device routingSource
 
-busctl set-property "${busname}" /soundcraft/utils/notepad/0 \
+${busctl} set-property "${busname}" /soundcraft/utils/notepad/0 \
        soundcraft.utils.notepad.device routingSource s INPUT_7_8
